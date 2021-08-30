c5162c6b17fb84b8d9a6e54989c900fd8ac3343a
CosminIlincariu <89630793+CosminIlincariu@users.noreply.github.com>
2021-08-30 17:43:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 const prevBtn = document.querySelector("#prev-btn");
 const nextBtn = document.querySelector("#next-btn");
 const book = document.querySelector(".book");
-
 const paper1 = document.querySelector("#p1");
-const paper2 = document.querySelector("#p2");
-
-let currentLocation = 1;
-let numOfPapers = 2;
-let maxLocation = 2;
-
-
 
 const openBook = () => {
     book.style.transform = "translateX(50%)";
-    prevBtn.style.transform = "translateX(-400px)";
+    prevBtn.style.transform = "translateX(-350px)";
     nextBtn.style.display = "none";
+    prevBtn.style.display = "block";
     paper1.classList.add("flipped");
 }
 
@@ -22,30 +15,61 @@ const closeBook = () => {
     book.style.transform = "translateX(0%)";
     prevBtn.style.transform= "translateX(0px)";
     nextBtn.style.display= "block";
-    paper1.classList.remove("flipped")
+    prevBtn.style.display= "none";
+    paper1.classList.remove("flipped");
 }
 
 prevBtn.addEventListener("click",closeBook);
 nextBtn.addEventListener("click",openBook);
 
-
 let list1 = [];
 	
-
-
-		const AddRow = () => {
+window.onload = () => {
+    let valori =  JSON.parse(localStorage.getItem("inregistrari"))
+    if (valori){
+        valori.map(rand => {
+            AddRow(rand)
+        })
+    }
+    
+}
+    
+		const AddRow = (row={}) => {
 
 			let AddRown = document.getElementById("show");
 			let NewRow = AddRown.insertRow(-1);
 
+            const numartelefon = row.telefon || document.getElementById("telefon").value
+            let mesaj = document.getElementById("message")
+            console.log(Object.keys(row).length)
+            if (Object.keys(row).length === 0) {
+
+           
+            if (!list1.every(prieten => numartelefon !== prieten.telefon))
+            {
+                mesaj.innerHTML = "Acest prieten este adaugat deja in agenda."
+                mesaj.classList.remove("approved-message");
+                mesaj.classList.add("error-message");
+                
+                return
+            } else {
+                
+                mesaj.innerHTML = "Prietenul a fost adaugat cu succes!"
+                mesaj.classList.remove("error-message")
+                mesaj.classList.add("approved-message")
+                
+            }
+        }
+
             list1.push({
-                nume: document.getElementById("nume").value,
-                prenume : document.getElementById("prenume").value,
-                telefon: document.getElementById("telefon").value,
-		        email :document.getElementById("email").value,
-                datanasterii: document.getElementById("datanasterii").value,
-                autovehicul : document.getElementById("autovehicul").value,
-                inghetata : document.getElementById("inghetata").value
+
+                nume: row.nume || document.getElementById("nume").value,
+                prenume : row.prenume || document.getElementById("prenume").value,
+                telefon: row.telefon || document.getElementById("telefon").value,
+		        email: row.email || document.getElementById("email").value,
+                datanasterii: row.datanasterii || document.getElementById("datanasterii").value,
+                autovehicul : row.autovehicul || document.getElementById("autovehicul").value,
+                inghetata : row.inghetata || document.getElementById("inghetata").value
                 
             })
 
@@ -66,9 +90,23 @@ let list1 = [];
 			cel6.innerHTML = list1[list1.length-1].autovehicul;
 			cel7.innerHTML = list1[list1.length-1].inghetata;
          
-      
-
+            if(Object.keys(row).length === 0) {
+                localStorage.setItem("inregistrari", JSON.stringify(list1))
+            }
 		}
 
-        const addBtn = document.getElementById("btn");
-        addBtn.addEventListener("click", AddRow);
+        const form = document.getElementById("myForm");
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            AddRow();
+        });
+
+        const clearBtn=document.getElementById("clearbtn");
+
+        clearBtn.addEventListener("click", () =>{
+            
+            localStorage.clear();
+            window.location.reload();
+
+           
+        })
