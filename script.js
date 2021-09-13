13bfa99d3745d6938168041545c9102a3ba00051
CosminIlincariu <89630793+CosminIlincariu@users.noreply.github.com>
2021-09-13 16:11:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,118 +1,117 @@
-
 const book = document.querySelector(".book");
 const paper1 = document.querySelector("#p1");
 const switchBtn = document.querySelector("#cb");
-
+const moduleWindow = document.querySelector(".module-agree");
+const overlay = document.querySelector(".overlay");
+const moduleBtnDa = document.querySelector(".module-button-one");
+const moduleBtnNu = document.querySelector(".module-button-two");
 
 const openBook = () => {
-    book.style.transform = "translateX(50%)";
-    paper1.classList.add("flipped");
-
-}
+  book.style.transform = "translateX(50%)";
+  paper1.classList.add("flipped");
+};
 
 const closeBook = () => {
-    book.style.transform = "translateX(0%)";
-    paper1.classList.remove("flipped");
-}
+  book.style.transform = "translateX(0%)";
+  paper1.classList.remove("flipped");
+};
 
-
-
-switchBtn.addEventListener("click",() => {
-    if(switchBtn.checked===true)
-    {
-        openBook();
-
-    }
-    else {
-        closeBook();
-    }
-})
+switchBtn.addEventListener("click", () => {
+  if (switchBtn.checked === true) {
+    openBook();
+  } else {
+    closeBook();
+  }
+});
 
 let list1 = [];
-	
+
 window.onload = () => {
-    let valori =  JSON.parse(localStorage.getItem("inregistrari"))
-    if (valori){
-        valori.map(rand => {
-            AddRow(rand)
-        })
+  let valori = JSON.parse(localStorage.getItem("inregistrari"));
+  if (valori) {
+    valori.map((rand) => {
+      AddRow(rand);
+    });
+  }
+};
+
+const AddRow = (row = {}) => {
+  let AddRown = document.getElementById("show");
+  let NewRow = AddRown.insertRow(-1);
+
+  const numartelefon = row.telefon || document.getElementById("telefon").value;
+  let mesaj = document.getElementById("message");
+  console.log(Object.keys(row).length);
+  if (Object.keys(row).length === 0) {
+    if (!list1.every((prieten) => numartelefon !== prieten.telefon)) {
+      mesaj.innerHTML = "Acest prieten este adaugat deja in agenda.";
+      mesaj.classList.remove("approved-message");
+      mesaj.classList.add("error-message");
+
+      return;
+    } else {
+      mesaj.innerHTML = "Prietenul a fost adaugat cu succes!";
+      mesaj.classList.remove("error-message");
+      mesaj.classList.add("approved-message");
     }
-    
-}
-    
-		const AddRow = (row={}) => {
+  }
 
-			let AddRown = document.getElementById("show");
-			let NewRow = AddRown.insertRow(-1);
+  list1.push({
+    nume: row.nume || document.getElementById("nume").value,
+    prenume: row.prenume || document.getElementById("prenume").value,
+    telefon: row.telefon || document.getElementById("telefon").value,
+    email: row.email || document.getElementById("email").value,
+    datanasterii:
+      row.datanasterii || document.getElementById("datepicker").value,
+    autovehicul:
+      row.autovehicul || document.getElementById("autovehicul").value,
+    inghetata: row.inghetata || document.getElementById("inghetata").value,
+  });
 
-            const numartelefon = row.telefon || document.getElementById("telefon").value
-            let mesaj = document.getElementById("message")
-            console.log(Object.keys(row).length)
-            if (Object.keys(row).length === 0) {
+  let cel1 = NewRow.insertCell(0);
+  let cel2 = NewRow.insertCell(1);
+  let cel3 = NewRow.insertCell(2);
+  let cel4 = NewRow.insertCell(3);
+  let cel5 = NewRow.insertCell(4);
+  let cel6 = NewRow.insertCell(5);
+  let cel7 = NewRow.insertCell(6);
 
-           
-            if (!list1.every(prieten => numartelefon !== prieten.telefon))
-            {
-                mesaj.innerHTML = "Acest prieten este adaugat deja in agenda."
-                mesaj.classList.remove("approved-message");
-                mesaj.classList.add("error-message");
-                
-                return
-            } else {
-                
-                mesaj.innerHTML = "Prietenul a fost adaugat cu succes!"
-                mesaj.classList.remove("error-message")
-                mesaj.classList.add("approved-message")
-                
-            }
-        }
+  cel1.innerHTML = list1[list1.length - 1].nume;
+  cel2.innerHTML = list1[list1.length - 1].prenume;
+  cel3.innerHTML = list1[list1.length - 1].telefon;
+  cel4.innerHTML = list1[list1.length - 1].email;
+  cel5.innerHTML = list1[list1.length - 1].datanasterii;
+  cel6.innerHTML = list1[list1.length - 1].autovehicul;
+  cel7.innerHTML = list1[list1.length - 1].inghetata;
 
-            list1.push({
+  if (Object.keys(row).length === 0) {
+    localStorage.setItem("inregistrari", JSON.stringify(list1));
+  }
+};
 
-                nume: row.nume || document.getElementById("nume").value,
-                prenume : row.prenume || document.getElementById("prenume").value,
-                telefon: row.telefon || document.getElementById("telefon").value,
-		        email: row.email || document.getElementById("email").value,
-                datanasterii: row.datanasterii || document.getElementById("datanasterii").value,
-                autovehicul : row.autovehicul || document.getElementById("autovehicul").value,
-                inghetata : row.inghetata || document.getElementById("inghetata").value
-                
-            })
+const form = document.getElementById("myForm");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  AddRow();
+});
 
-			let cel1 = NewRow.insertCell(0);
-			let cel2 = NewRow.insertCell(1);
-			let cel3 = NewRow.insertCell(2);
-		    let cel4 = NewRow.insertCell(3);
-            let cel5= NewRow.insertCell(4);
-            let cel6 = NewRow.insertCell(5);
-            let cel7 = NewRow.insertCell(6);
+const clearBtn = document.getElementById("clearbtn");
 
+clearBtn.addEventListener("click", () => {
+  moduleWindow.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 
-			cel1.innerHTML = list1[list1.length-1].nume;
-			cel2.innerHTML = list1[list1.length-1].prenume;
-			cel3.innerHTML = list1[list1.length-1].telefon;
-			cel4.innerHTML = list1[list1.length-1].email;
-			cel5.innerHTML = list1[list1.length-1].datanasterii;
-			cel6.innerHTML = list1[list1.length-1].autovehicul;
-			cel7.innerHTML = list1[list1.length-1].inghetata;
-         
-            if(Object.keys(row).length === 0) {
-                localStorage.setItem("inregistrari", JSON.stringify(list1))
-            }
-		}
+  moduleBtnDa.addEventListener("click", () => {
+    localStorage.clear();
+    window.location.reload();
+  });
 
-        const form = document.getElementById("myForm");
-        form.addEventListener('submit', (event) => {
-            event.preventDefault();
-            AddRow();
-        });
+  moduleBtnNu.addEventListener("click", () => {
+    moduleWindow.classList.add("hidden");
+    overlay.classList.add("hidden");
+  });
+});
 
-        const clearBtn=document.getElementById("clearbtn");
-
-        clearBtn.addEventListener("click", () =>{
-            
-            localStorage.clear();
-            window.location.reload();
-
-           
-        })
+$(function () {
+  $("#datepicker").datepicker();
+});
